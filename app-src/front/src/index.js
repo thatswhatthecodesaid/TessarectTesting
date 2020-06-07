054c32ab03c344946c4883becc1c6bab6a9215d2
Pushpendra Vishwakarma <Pushpendra.hpx2001@gmail.com>
2020-06-07 18:20:15 +0530

    Going for Test

--- app-src/front/src/index.js
+++ app-src/front/src/index.js
@@ -11,7 +11,7 @@ import allReducers from './Reducers/index';
 
 const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-
+console.log(store)
 
 ReactDOM.render(
   <Provider store={store}> 
